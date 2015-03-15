d02b0780b20e374455b8fda1b84180c9ec590d78
Donhilion <donhilion@googlemail.com>
2015-03-15 22:20:48 +0100

    Closing event fixed.

--- src/de/donhilion/multiclipboard/gui/MainWindow.java
+++ src/de/donhilion/multiclipboard/gui/MainWindow.java
@@ -28,7 +28,7 @@ public class MainWindow {
 
         window.addWindowListener(new WindowAdapter() {
             @Override
-            public void windowClosed(WindowEvent e) {
+            public void windowClosing(WindowEvent e) {
                 super.windowClosed(e);
 
                 if (onCloseListener != null) {
